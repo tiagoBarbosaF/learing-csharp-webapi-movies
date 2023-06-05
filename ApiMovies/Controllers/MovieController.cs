b1285e8436295b16b729dd1452ebe623585c5e4c
Tiago Barbosa <tiago@farias.cc>
2023-06-05 16:11:33 -0300

    initial commit, creating an web api using .net 6

---
+++ ApiMovies/Controllers/MovieController.cs
@@ -0,0 +1,47 @@
+﻿using ApiMovies.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMovies.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> _movies = new()
+    {
+        new Movie("The Matrix", "Action", 136),
+        new Movie("Inception", "Science Fiction", 148),
+        new Movie("Pulp Fiction", "Crime", 154),
+        new Movie("The Shawshank Redemption", "Drama", 142),
+        new Movie("The Dark Knight", "Action", 152),
+        new Movie("Fight Club", "Drama", 139),
+        new Movie("Goodfellas", "Crime", 146),
+        new Movie("Interstellar", "Science Fiction", 169),
+        new Movie("The Godfather", "Crime", 175),
+        new Movie("The Lord of the Rings: The Fellowship of the Ring", "Fantasy", 178)
+    };
+
+    [HttpPost]
+    public IActionResult AddMovie([FromBody] Movie movie)
+    {
+        _movies.Add(movie);
+        return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id }, movie);
+    }
+
+    [HttpGet]
+    public IEnumerable<Movie> GetAllMovies([FromQuery] int skip = 0, [FromQuery] int take = 5)
+    {
+        return _movies.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetMovieById(Guid id)
+    {
+        var movie = _movies.FirstOrDefault(movie => movie.Id == id);
+
+        if (movie == null)
+            return NotFound();
+
+        return Ok(movie);
+    }
+}
---
+++ ApiMovies/Models/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace ApiMovies.Models;
+
+public class Movie
+{
+    public Guid Id { get; set; }
+    
+    [Required(ErrorMessage = "Title is required.")]
+    public string Title { get; set; }
+    
+    [Required(ErrorMessage = "Gender is required")]
+    [MaxLength(50,ErrorMessage = "Length of Gender cannot bigger than 50 characters.")]
+    public string Gender { get; set; }
+    
+    [Required(ErrorMessage = "Duration is required")]
+    [Range(60,600,ErrorMessage = "Duration of movie is cannot lower than 60 min and not bigger than 600 min.")]
+    public int Duration { get; set; }
+
+    public Movie(string title, string gender, int duration)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Gender = gender;
+        Duration = duration;
+    }
+}
